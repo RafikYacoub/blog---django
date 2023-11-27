86da1e9e67ba65c9558c9750b0c69dc6e1445fa2
Rafik Yacoub <rafikyacoub@aucegypt.edu>
2023-11-27 22:00:26 +0200

    added a front page and an about page

--- blog_project/core/views.py
+++ blog_project/core/views.py
@@ -4,4 +4,8 @@ from django.shortcuts import render
 
 
 def frontpage(request):
-    return render(request, "core/base.html")
+    return render(request, "core/frontpage.html")
+
+
+def about(request):
+    return render(request, "core/about.html")
